5c436d48ba5c0bed3f067ddfe51eaec8e04ed1c8
AsafKed <asafkedem97@gmail.com>
2020-12-23 00:41:25 +0100

    Centered containers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+
+const theme = {
+  bg: '#F2EEEE',
+  primary: '#16262E',
+  secondary: '#656566',
+  fade: '#E7E4E4'
+};
+
+const Application = styled.div`
+  background: ${theme.bg};
+  height: 100vh;
+  /* TODO add font family */
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+// TODO design different sizes in Figma
+const Container = styled.div`
+  height: 450px;
+  width: 740px;
+  margin: 10px;
+  background: red;
+  color: ${theme.primary};
+  border: 3px solid ${theme.primary};
+`;
+
+// For the main name
+const H1 = styled.h1`
+
+`;
+
+// For the side list
+const H3 = styled.h3`
+
+`;
+
+// For the subtitle 
+const H5 = styled.h5`
+
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Application>
+        <Container>Container 1</Container>
+        <Container>Container 2</Container>
+      </Application>
+    </ThemeProvider>
   );
 }
 
