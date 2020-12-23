6d27948d3ea034e23bee4723926a0c0452f8391e
AsafKed <asafkedem97@gmail.com>
2020-12-23 01:51:54 +0100

    Basic styling

--- src/App.js
+++ src/App.js
@@ -4,42 +4,68 @@ const theme = {
   bg: '#F2EEEE',
   primary: '#16262E',
   secondary: '#656566',
-  fade: '#E7E4E4'
+  faded: '#E7E4E4'
 };
 
 const Application = styled.div`
+  /* Set background */
   background: ${theme.bg};
   height: 100vh;
-  /* TODO add font family */
+
+  /* Set centering */
   display: flex;
   justify-content: space-around;
   align-items: center;
   flex-wrap: wrap;
+
+
+  /* Font size (set up for rem usage) */
+  font-size: 62.5%;
+  /* So that 1rem = 10px */
 `;
 
 // TODO design different sizes in Figma
+// TODO enable different centering options (middle, left, right)
+// TODO enable border and no border
 const Container = styled.div`
+  /* Create container shape */
   height: 450px;
   width: 740px;
   margin: 10px;
-  background: red;
-  color: ${theme.primary};
-  border: 3px solid ${theme.primary};
+  
+  border: 6px solid ${theme.primary};
+
+  /* Center items within it */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 // For the main name
 const H1 = styled.h1`
-
+  font-family: 'Tw Cen MT';
+  font-size: 5rem;
+  text-transform: uppercase;
 `;
 
 // For the side list
-const H3 = styled.h3`
+const H3 = styled.a`
+  font-family: Mainframe, serif;
+  font-size: 3rem;
 
+  color: ${theme.faded};
+  &:hover {
+    color: ${theme.secondary};
+  };
+  text-decoration: none;
 `;
 
 // For the subtitle 
 const H5 = styled.h5`
-
+  font-family: Sanchez;
+  font-size: 1.4rem;
+  color: #000;
 `;
 
 
@@ -48,8 +74,15 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Application>
-        <Container>Container 1</Container>
-        <Container>Container 2</Container>
+        <Container border="true">
+          <H1>Asaf Kedem</H1>
+          <H5>This is my website!</H5>
+        </Container>
+        <Container>
+          <H3 href="projects">Projects</H3>
+          <H3 href="skills">Skills</H3>
+          <H3 href="blog">Entrepreneurial Blog</H3>
+        </Container>
       </Application>
     </ThemeProvider>
   );
