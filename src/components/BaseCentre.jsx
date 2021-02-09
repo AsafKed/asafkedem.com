9c7666eaed4538bff31100faeb5fb6f4b093d8f1
AsafKed <asafkedem97@gmail.com>
2021-02-09 13:37:36 +0100

    Something

--- src/components/BaseCentre.jsx
+++ src/components/BaseCentre.jsx
@@ -8,17 +8,36 @@ import SkillContainer from './SkillContainer.jsx';
 import me from '../media/me.jpg';
 import BaseHeader from './BaseHeader.jsx';
 
+// let Internals = (props) => {
+//     const page = props.page;
+//     let internal = (<div>No page loaded</div>);
+//     if (page === "about") {
+//         internal = (<div>About</div>);
+//     }
+//     else if (page === "skills") {
+//         internal = (<div>Skills</div>);
+//     }
+//     else {
+//         internal = (<div>Page doesn't exist!</div>);
+//     }
+//     return (<div>Test</div>);
+// }
+
+function Testy() {
+    return (<div>Why</div>);
+}
 class BaseCentre extends React.Component {
     render() {
-        let comp = Internals(this.props.page)
         return (<div>
             <Page>
                 <SubPage> 
                     <BaseHeader />
+                    <Testy/>
                     {/* <Internals page={this.props.page}/> */}
-                    <Wrapper>
+                    <LongText>{this.props.page}</LongText>
+                    {/* <Wrapper>
                         <LongText> {this.props.text} </LongText>
-                    </Wrapper>
+                    </Wrapper> */}
 
                 </SubPage>
             </Page>
@@ -30,16 +49,3 @@ class BaseCentre extends React.Component {
 
 export default BaseCentre;
 
-function Internals(page) {
-    let internal = <div>No page loaded</div>;
-    if (page === "about") {
-        internal = <div>About</div>
-    }
-    else if (page === "skills") {
-        internal = <div>Skills</div>
-    }
-    else {
-        internal = <div>Page doesn't exist!</div>
-    }
-    return (internal);
-}
